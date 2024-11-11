68d8c7fc9583f46f0bab00680890884c29eae1a3
Dmitriy Rackin <dmitriirackin888@ya.ru>
2024-11-11 20:33:52 +0400

    base + main page makeup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,96 @@
 <script>
 	import '../app.css';
 	let { children } = $props();
+
+	import coin from '$lib/coin.png';
+	import missions from '$lib/missions.png';
+	import shop from '$lib/shop.png';
+	import friends from '$lib/friends.png';
+	import boosts from '$lib/boosts.png';
 </script>
 
+<div class="relative h-[926px] w-[428px] bg-[#1c1f24]">
+	<div
+		class="absolute left-0 top-0 h-[245px] w-[428px] bg-gradient-to-b from-black to-[#ffbd20]"
+	></div>
+	<div
+		class="absolute left-[18px] top-[145px] h-[703.61px] w-[392px] rounded-[50px] bg-[#ffbd20]"
+	></div>
+	<div
+		class="absolute left-0 top-[150px] h-[776px] w-[428px] rounded-tl-[45px] rounded-tr-[45px] bg-black"
+	></div>
+	<div class="absolute left-[30px] top-[822px] h-[76px] w-[368px]">
+		<div class="absolute top-[9px] h-[67px] w-[368px] rounded-[15px] bg-[#111111]"></div>
+		<a href="/">
+			<div class="absolute left-[160px] top-0 h-[65px] w-12">
+				<img
+					class="absolute left-0 top-0 h-12 w-12 rounded-[256px] object-cover"
+					alt="coin menu button"
+					src={coin}
+				/>
+				<div
+					class="absolute left-[2px] top-[56px] font-['Roboto'] text-[9px] font-medium leading-[9px] text-white"
+				>
+					Добывать
+				</div>
+			</div>
+		</a>
+		<a href="missions">
+			<div class="absolute left-[233px] top-[19px] h-[46px] w-[37px]">
+				<div
+					class="absolute left-0 top-[37px] font-['Roboto'] text-[9px] font-medium leading-[9px] text-white"
+				>
+					Задания
+				</div>
+				<img
+					class="absolute left-[1px] top-0 h-[34px] w-[34px] object-cover"
+					alt="missions"
+					src={missions}
+				/>
+			</div>
+		</a>
+		<a href="/boosts">
+			<div class="absolute left-[30px] top-[19px] h-[46px] w-[34px]">
+				<div
+					class="absolute left-[4px] top-[37px] font-['Roboto'] text-[9px] font-medium leading-[9px] text-white"
+				>
+					Бусты
+				</div>
+				<img
+					class="absolute left-0 top-0 h-[34px] w-[34px] object-cover"
+					alt="boosts"
+					src={boosts}
+				/>
+			</div>
+		</a>
+		<a href="/shop">
+			<div class="absolute left-[302px] top-[19px] h-[46px] w-[37px]">
+				<div
+					class="absolute left-0 top-[37px] font-['Roboto'] text-[9px] font-medium leading-[9px] text-white"
+				>
+					Магазин
+				</div>
+				<img
+					class="absolute left-[1px] top-0 h-[34px] w-[34px] object-cover"
+					alt="shop"
+					src={shop}
+				/>
+			</div>
+		</a>
+		<a href="/friends">
+			<div class="absolute left-[99px] top-[19px] h-[46px] w-[34px]">
+				<div
+					class="absolute left-[2px] top-[37px] font-['Roboto'] text-[9px] font-medium leading-[9px] text-white"
+				>
+					Друзья
+				</div>
+				<img
+					class="absolute left-0 top-0 h-[34px] w-[34px] object-cover"
+					alt="friends"
+					src={friends}
+				/>
+			</div>
+		</a>
+	</div>
+</div>
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,62 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import avatar from '$lib/avatar.jpg';
+	import coin from '$lib/coin.png';
+	import energy from '$lib/energy.png';
+</script>
+
+<div>
+	<img
+		class="absolute left-[30px] top-[97px] h-[33px] w-[33px] rounded-[7px] object-cover"
+		alt="avatar"
+		src={avatar}
+	/>
+	<div
+		class="absolute left-[73px] top-[107px] font-['Roboto'] text-sm font-semibold leading-[14px] text-white"
+	>
+		surgeon.design
+	</div>
+	<div class="absolute left-[115px] top-[310px] inline-flex items-center justify-center gap-2">
+		<img class="h-[42px] w-[42px] rounded-[256px] object-cover" alt="coin icon" src={coin} />
+		<div class="font-['Roboto'] text-[40px] font-bold leading-[44px] text-[#fffefc]">507,981</div>
+	</div>
+	<div
+		class="absolute left-[70px] top-[413px] h-[289px] w-[289px] rounded-full bg-[#ffbd20] opacity-60 blur-[100px]"
+	></div>
+	<img
+		class="absolute left-[55px] top-[399px] h-[318px] w-[318px] rounded-[256px] object-cover"
+		alt="big coin"
+		src={coin}
+	/>
+	<div
+		class="absolute left-[30px] top-[180px] h-[65px] w-[368px] rounded-[15px] bg-[#101010]"
+	></div>
+	<div class="absolute left-[272px] top-[195px] h-9 w-[101px] rounded-[11px] bg-[#ffbd20]"></div>
+	<div class="absolute left-[55px] top-[192px] font-['Roboto'] text-xs font-medium text-white">
+		Прибыль в час
+	</div>
+	<div class="absolute left-[287px] top-[205px] font-['Roboto'] text-sm font-medium text-black">
+		Увеличить
+	</div>
+	<img
+		class="absolute left-[55px] top-[211px] h-[22px] w-[22px] rounded-[256px] object-cover"
+		alt="coin icon"
+		src={coin}
+	/>
+	<div
+		class="absolute left-[82px] top-[215px] font-['Roboto'] text-sm font-semibold leading-none text-white"
+	>
+		500k
+	</div>
+	<div class="absolute left-[30px] top-[787px] h-[27px] w-[368px]">
+		<div class="absolute left-[18px] top-[2px] font-['Roboto'] text-sm font-semibold text-white">
+			400/500
+		</div>
+		<div class="absolute left-[313px] top-[2px] font-['Roboto'] text-sm font-semibold text-white">
+			Энергия
+		</div>
+		<div class="absolute left-0 top-0 h-[27px] w-3.5">
+			<img class="absolute left-0 top-0 h-[27px] object-cover" alt="energy" src={energy} />
+		</div>
+		<div class="absolute left-0 top-[1px] h-[26px] w-[13px]"></div>
+	</div>
+</div>
