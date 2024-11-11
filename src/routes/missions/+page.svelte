c61f1eed20c07bab275bbfed2d1c3a39d12ed1bf
Dmitriy Rackin <dmitriirackin888@ya.ru>
2024-11-11 22:22:35 +0400

    missions page makeup

---
+++ src/routes/missions/+page.svelte
@@ -0,0 +1,135 @@
+<script>
+	import tg from '$lib/tg.png';
+	import coin from '$lib/coin.png';
+	import arrow from '$lib/arrow.svg';
+
+	import Popup from './Popup.svelte';
+</script>
+
+<div
+	class="absolute
+    left-[61px] top-[180px] text-center font-['Roboto'] text-3xl font-bold text-[#fffefc]"
+>
+	Зарабатывай больше<br />Brawl Coin
+</div>
+<div class="absolute left-[30px] top-[309px] h-[75px] w-[368px]">
+	<div class="absolute left-0 top-0 h-[75px] w-[368px] rounded-[20px] bg-[#101010]"></div>
+	<div class="absolute left-[20px] top-[15px] h-[45px] w-[297px]">
+		<div
+			class="absolute left-[55px] top-[2px] w-[250px] font-['Roboto'] text-xs font-medium text-white"
+		>
+			Присоединяйся к нашему Telegram каналу
+		</div>
+		<img
+			class="absolute left-[55px] top-[21px] h-[22px] w-[22px] rounded-[256px] object-cover"
+			alt="coin icon"
+			src={coin}
+		/>
+		<div
+			class="absolute left-[82px] top-[25px] font-['Roboto'] text-sm font-semibold leading-[14px] text-[#ffbd20]"
+		>
+			+1
+		</div>
+		<img class="absolute left-0 top-0 h-[45px] w-[45px] object-cover" alt="tg" src={tg} />
+		<img class="absolute right-[-30px] top-[17px]" src={arrow} alt="arrow" />
+	</div>
+</div>
+<div class="absolute left-[30px] top-[394px] h-[75px] w-[368px]">
+	<div class="absolute left-0 top-0 h-[75px] w-[368px] rounded-[20px] bg-[#101010]"></div>
+	<div class="absolute left-[20px] top-[15px] h-[45px] w-[297px]">
+		<div
+			class="absolute left-[55px] top-[2px] w-[250px] font-['Roboto'] text-xs font-medium text-white"
+		>
+			Присоединяйся к нашему Telegram каналу
+		</div>
+		<img
+			class="absolute left-[55px] top-[21px] h-[22px] w-[22px] rounded-[256px] object-cover"
+			alt="coin icon"
+			src={coin}
+		/>
+		<div
+			class="absolute left-[82px] top-[25px] font-['Roboto'] text-sm font-semibold leading-[14px] text-[#ffbd20]"
+		>
+			+1
+		</div>
+		<img class="absolute left-0 top-0 h-[45px] w-[45px] object-cover" alt="tg" src={tg} />
+		<img class="absolute right-[-30px] top-[17px]" src={arrow} alt="arrow" />
+	</div>
+</div>
+<div class="absolute left-[30px] top-[479px] h-[75px] w-[368px]">
+	<div class="absolute left-0 top-0 h-[75px] w-[368px] rounded-[20px] bg-[#101010]"></div>
+	<div class="absolute left-[20px] top-[15px] h-[45px] w-[297px]">
+		<div
+			class="absolute left-[55px] top-[2px] w-[250px] font-['Roboto'] text-xs font-medium text-white"
+		>
+			Присоединяйся к нашему Telegram каналу
+		</div>
+		<img
+			class="absolute left-[55px] top-[21px] h-[22px] w-[22px] rounded-[256px] object-cover"
+			alt="coin icon"
+			src={coin}
+		/>
+		<div
+			class="absolute left-[82px] top-[25px] font-['Roboto'] text-sm font-semibold leading-[14px] text-[#ffbd20]"
+		>
+			+1
+		</div>
+		<img class="absolute left-0 top-0 h-[45px] w-[45px] object-cover" alt="tg" src={tg} />
+		<img class="absolute right-[-30px] top-[17px]" src={arrow} alt="arrow" />
+	</div>
+</div>
+<div class="absolute left-[30px] top-[613px] h-[75px] w-[368px]">
+	<div class="absolute left-0 top-0 h-[75px] w-[368px] rounded-[20px] bg-[#101010]"></div>
+	<div class="absolute left-[20px] top-[15px] h-[45px] w-[297px]">
+		<div
+			class="absolute left-[55px] top-[2px] w-[250px] font-['Roboto'] text-xs font-medium text-white"
+		>
+			Присоединяйся к нашему Telegram каналу
+		</div>
+		<img
+			class="absolute left-[55px] top-[21px] h-[22px] w-[22px] rounded-[256px] object-cover"
+			alt="coin icon"
+			src={coin}
+		/>
+		<div
+			class="absolute left-[82px] top-[25px] font-['Roboto'] text-sm font-semibold leading-[14px] text-[#ffbd20]"
+		>
+			+1
+		</div>
+		<img class="absolute left-0 top-0 h-[45px] w-[45px] object-cover" alt="tg" src={tg} />
+		<img class="absolute right-[-30px] top-[17px]" src={arrow} alt="arrow" />
+	</div>
+</div>
+<div class="absolute left-[30px] top-[698px] h-[75px] w-[368px]">
+	<div class="absolute left-0 top-0 h-[75px] w-[368px] rounded-[20px] bg-[#101010]"></div>
+	<div class="absolute left-[20px] top-[15px] h-[45px] w-[297px]">
+		<div
+			class="absolute left-[55px] top-[2px] w-[250px] font-['Roboto'] text-xs font-medium text-white"
+		>
+			Присоединяйся к нашему Telegram каналу
+		</div>
+		<img
+			class="absolute left-[55px] top-[21px] h-[22px] w-[22px] rounded-[256px] object-cover"
+			alt="coin icon"
+			src={coin}
+		/>
+		<div
+			class="absolute left-[82px] top-[25px] font-['Roboto'] text-sm font-semibold leading-[14px] text-[#ffbd20]"
+		>
+			+1
+		</div>
+		<img class="absolute left-0 top-0 h-[45px] w-[45px] object-cover" alt="tg" src={tg} />
+		<img class="absolute right-[-30px] top-[17px]" src={arrow} alt="arrow" />
+	</div>
+</div>
+<div
+	class="absolute left-[30px] top-[280px] font-['Roboto'] text-sm font-semibold leading-[14px] text-white"
+>
+	Ежедневные заданий
+</div>
+<div
+	class="absolute left-[30px] top-[584px] font-['Roboto'] text-sm font-semibold leading-[14px] text-white"
+>
+	Список заданий
+</div>
+<!-- <Popup /> -->
